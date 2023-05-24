c740e695e2689a25e7c092ecc9552a7fb4c7bcaa
Deyan Delchev <60808936+lastvoidtemplar@users.noreply.github.com>
2023-05-24 14:43:00 +0300

    add layer functionalities

--- network/src/lib.rs
+++ network/src/lib.rs
@@ -1,14 +1,21 @@
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
+mod layer;
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    use crate::layer::*;
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        
+        let mut layer:Layer = Layer::new(2, 3, ActivationFunction::Sigmoid);
+        let output = layer.forward(vec![1.0,2.0,1.0]);
+        layer.backwards(vec![1.0,2.0,1.0]);
+        layer.backwards(vec![0.5,0.0,0.25]);
+        layer.apply_gradiant(1.0);
+        assert_eq!(4, 4);
     }
 }
